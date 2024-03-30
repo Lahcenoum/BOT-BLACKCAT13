b16e1621a55ff445e0aea8fbb351f05770f716c1
Lahcenoum <104395664+Lahcenoum@users.noreply.github.com>
2024-03-30 07:09:07 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,17 +17,17 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.owner = ['201098906252'] //ur owner number
 global.ownername = "الأقط الاسود13❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
+global.ytname = "YT: FASTVPSVIP" //ur yt chanel name
 global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.location = "Africa, maroc, Cairo" //ur location
 
 //bot bomdy 
 global.ownernomer = "201098906252" //ur number
 global.premium = ['201098906252'] //ur premium number
 global.botname = '❤️13القط الاسود ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.linkz = "https://t.me/FASTVPSVIP" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://t.me/FASTVPSVIP' //script link
 global.themeemoji = "♥" //ur theme emoji
 global.packname = "⍣ا االقط الاسود13 يروحي⍣" //ur sticker watermark packname
 global.author = "⍣01098906252⍣" //ur sticker watermark author
